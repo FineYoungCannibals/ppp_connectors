f4b79b6e022144c2dbaf6a8b0c3c4db44f2c17f9
Rob D'Aveta <rob.daveta@gmail.com>
2025-12-16 11:09:10 -0500

    Remove legacy ppp_connectors package and test suite

---
+++ dev_env/api/test_ipqs.py
@@ -0,0 +1,28 @@
+from apiary.api_connectors.ipqs import AsyncIPQSConnector, IPQSConnector
+import asyncio
+from apiary.helpers import combine_env_configs, setup_logger
+from typing import Dict, Any
+import json
+
+
+env_config: Dict[str, Any] = combine_env_configs()
+
+async def main():
+    ipqs = AsyncIPQSConnector(
+        api_key=env_config["IPQS_API_KEY"],
+        enable_logging=True,
+        load_env_vars=True,
+        trust_env=True,
+        verify=False
+    )
+
+    res = await ipqs.malicious_url(
+        query="cracked.to"
+    )
+
+    print(res)
+    print(res.json())
+    print(res.headers)
+
+asyncio.run(main())
+# main()
